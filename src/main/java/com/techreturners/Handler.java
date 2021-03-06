c9cc028b0e7bc2b6a5e619173b4d7e3d99396ac8
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-06 12:46:50 +0000

    creating Task class & adding to Handler.java

--- src/main/java/com/techreturners/Handler.java
+++ src/main/java/com/techreturners/Handler.java
@@ -1,8 +1,11 @@
-package com.serverless;
+package com.techreturners;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import com.techreturners.model.Task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,11 +18,28 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: {}", input);
-		Response responseBody = new Response("Hello World!", input);
+
+		// this is returned when run serverless -f tasks.api
+		LOG.info("request received: {}", input);
+//		Response responseBody = new Response("Hello World!", input);
+
+		Task t1 = new Task("abc123", "Pick up newspapers", false );
+		Task t2 = new Task("abc1234" , "Learn Java!", false);
+
+/*		// generating an error - to check logs
+
+		Task t3 = null;
+		System.out.println(t3.getDescription());
+*/
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(t1);
+		tasks.add(t2);
+
+
+		// builder will convert to JSON to send the response
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
-				.setObjectBody(responseBody)
+				.setObjectBody(tasks)
 				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
 				.build();
 	}
