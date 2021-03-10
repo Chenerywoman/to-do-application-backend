ff8e62f87389ffc653eaa33face1c89a912f6a2a
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-10 22:07:19 +0000

    following TR code along to create connection to RDB tasks database

--- src/main/java/com/techreturners/GetTasksHandler.java
+++ src/main/java/com/techreturners/GetTasksHandler.java
@@ -1,5 +1,9 @@
 package com.techreturners;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,13 +20,17 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class GetTasksHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	private static final Logger LOG = LogManager.getLogger(GetTasksHandler.class);
+    private static final Logger LOG = LogManager.getLogger(GetTasksHandler.class);
 
-	@Override
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+    private Connection connection = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
 
-		// this is returned when run serverless -f tasks.api
-		LOG.info("request received: {}");
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+
+        // this is returned when run serverless -f tasks.api
+        LOG.info("request received");
 
 		/* if want to use one handler for two methods:
 			String requestMethod = request.getHttpMethod();
@@ -30,42 +38,91 @@ public class GetTasksHandler implements RequestHandler<APIGatewayProxyRequestEve
 			if post - save task, else get tasks
 		*/
 
-		String userId = request.getPathParameters().get("userId");
+        String UserId = request.getPathParameters().get("userId");
 
-		List<Task> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
-		if (userId.equals("abc123")){
-			Task t1 = new Task("abc123", "Pick up newspapers", false );
-			tasks.add(t1);
-		} else {
-			Task t2 = new Task("abc1234" , "Learn Java!", false);
-			tasks.add(t2);
+        try {
+            // need to refactor to put the connection to the database in a different class as will need it for each SQL query
+
+            // instantiate the sql driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            LOG.debug(String.format("Connecting to DB on %s", System.getenv("DB_HOST")));
+
+            // instantiate the connection
+            connection = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s?user=%s&password=%s",
+                    System.getenv("DB_HOST"),
+                    System.getenv("DB_NAME"),
+                    System.getenv("DB_USER"),
+                    System.getenv("DB_PASSWORD")));
+
+            preparedStatement = connection.prepareStatement("SELECT * FROM tasks WHERE UserId = ?");
+            preparedStatement.setString(1, UserId);
+            resultSet = preparedStatement.executeQuery();
+
+            // this gets each row of the returned data
+            while (resultSet.next()) {
+                Task t1 = new Task(resultSet.getString("taskId"),
+                        resultSet.getString("description"),
+                        resultSet.getBoolean("completed"));
+
+                tasks.add(t1);
+
+            }
+
+
+        } catch (Exception e) {
+            LOG.error(String.format("Unable to query database for tasks for user %s", UserId), e);
+        } finally {
+			// close the connection when it is finished, to stop using up connections
+
+			closeConnection();
 		}
 
-
-/*		// generating an error - to check logs
-
-		Task t3 = null;
-		System.out.println(t3.getDescription());
-*/
 
 		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-		response.setStatusCode(200);
+        response.setStatusCode(200);
 
-		// cannot put tasks array list straight into the response;
-		// need to use ObjectMapper dependency to change to a JSON object
-		ObjectMapper objectMapper = new ObjectMapper();
+        // cannot put tasks array list straight into the response;
+        // need to use ObjectMapper dependency to change to a JSON object
+        ObjectMapper objectMapper = new ObjectMapper();
 
-		// need to have in a try catch, or code will error
-		try {
-			// sets the tasks Array to a string (json) object
-			String responseBody = objectMapper.writeValueAsString(tasks);
-			// adds json string responseBody to the body of the response
-			response.setBody(responseBody);
+        // need to have in a try catch, in case code errors
+        try {
+            // sets the tasks Array to a string (json) object
+            String responseBody = objectMapper.writeValueAsString(tasks);
+            // adds json string responseBody to the body of the response
+            response.setBody(responseBody);
 
-		} catch (JsonProcessingException error){
-			LOG.error("Unable to marshal tasks array");
-		}
-		return response;
-	}
+        } catch (JsonProcessingException error) {
+            LOG.error("Unable to marshal tasks array", error);
+        }
+
+        return response;
+    }
+
+    // separate function to close connection - put in own reusable class, so the handler calls a different class to get the data & represent it as JSON
+    private void closeConnection() {
+        // close the connection when it is finished, to stop using up connections
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+
+        } catch (Exception e) {
+            LOG.error("Unable to close connection to MySQL - {}", e.getMessage());
+        }
+    }
+
 }
+
+
