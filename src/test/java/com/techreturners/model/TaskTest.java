767e360807b5ceede6df92060ad678cf30da20a7
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-12 15:58:47 +0000

    adding first test in TaskTest

--- src/test/java/com/techreturners/model/TaskTest.java
+++ src/test/java/com/techreturners/model/TaskTest.java
@@ -1,4 +1,21 @@
 package com.techreturners.model;
 
+//import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+// give human readable name to test
+import org.junit.jupiter.api.DisplayName;
+
 public class TaskTest {
+
+    @Test
+    @DisplayName("Test task description")
+    public void testTaskDescription(){
+        Task t = new Task("abc123", "Some task description", false);
+        // expected, actual, message if it fails
+         assertEquals("Some task description", t.getDescription(), "Task description was " +
+                "incorrect");
+    }
 }
+
